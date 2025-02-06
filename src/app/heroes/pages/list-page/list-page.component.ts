31b44cade4148f242729b33855074e9e968b0c35
Leticia Sepulveda <leticiasepulvedamancilla@gmail.com>
2025-02-06 13:23:27 +0100

    Subiendo cambios recientes

--- src/app/heroes/pages/list-page/list-page.component.ts
+++ src/app/heroes/pages/list-page/list-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../../interfaces/hero.interface';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-list-page',
@@ -11,8 +12,9 @@ import { Hero } from '../../interfaces/hero.interface';
 export class ListPageComponent implements OnInit{
 
 
+  private hero: Hero[] =[];
 
-  constructor(){}
+  constructor(private http: HttpClient){}
 
   ngOnInit(): void {
 
