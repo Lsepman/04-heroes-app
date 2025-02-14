f595eef77c08c5710d61229c03a134f845b44b73
Leticia Sepulveda <leticiasepulvedamancilla@gmail.com>
2025-02-14 19:20:08 +0100

    Trabajo local antes del pull

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -10,10 +10,29 @@ import { Publisher } from '../../interfaces/hero.interface';
 })
 export class NewPageComponent {
 
+  public heroForm = new FormGroup({
+    id: new FormControl(''),
+    superhero: new FormControl('',{nonNullable: true}),
+    publisher: new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego: new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img: new FormControl(''),
+
+
+  });
+
   public publishers =[
     {id: 'DC Comics', desc: 'DC - Comics'},
     {id: 'Marvel Comics', desc: 'Marvel - Comics'}
   ]
+
+  onSubmit(): void{
+    console.log({
+      formIsValid: this.heroForm.valid,
+      value: this.heroForm.value
+    });
+  }
 
 
 }
